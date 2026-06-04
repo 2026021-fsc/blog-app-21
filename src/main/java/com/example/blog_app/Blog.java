d7819834b8541300ce5c5b3929402911470f13bd
Numata <2026021@fsc-educ.com>
2026-06-04 15:04:02 +0900

    Blogクラスの追加

---
+++ src/main/java/com/example/blog_app/Blog.java
@@ -0,0 +1,11 @@
+package com.example.blog_app;
+
+public class Blog {
+    private final String name;
+    public Blog(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
